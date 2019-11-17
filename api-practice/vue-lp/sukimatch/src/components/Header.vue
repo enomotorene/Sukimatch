71280a972b10d5cefa1d8be325eeb9fc3a78b077
enomotorene <enomoto.rene@gmail.com>
2019-11-17 10:49:51 +0900

    modified text

--- api-practice/vue-lp/sukimatch/src/components/Header.vue
+++ api-practice/vue-lp/sukimatch/src/components/Header.vue
@@ -4,8 +4,8 @@
         <div class="container">
   <div class="row">
     <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-        <h4 class="sk-header-sub-text">バナー作成で手が足りない・・・</h4>
-        <h4 class="sk-header-sub-text">短納期大量発注・・・</h4>
+        <h4 class="sk-header-sub-text">「バナー作成で手が足りない・・・」</h4>
+        <h4 class="sk-header-sub-text">「短納期大量発注・・・」</h4>
           <h2>お困りの制作担当さんを<br>全国のクリエイターが<br>お手伝いします</h2>
            <form>
             <div class="form-row">
@@ -58,9 +58,5 @@ a.sk-btn-color {
 .navbar-brand {
     padding-right: 30px;
     padding-left: 30px;
-}
-
-h4.sk-header-sub-text {
-    color: #f25458;
 }
 </style>
